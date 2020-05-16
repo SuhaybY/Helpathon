098edc4046a4a24123ce7061b63303befd21378d
GurmeharS <gurmehar.sandhu@mail.utoronto.ca>
2020-05-16 14:41:44 -0400

    Changed hackathon class to construct from input object. Added accept and reject functionality.

--- src/components/HackathonView.js
+++ src/components/HackathonView.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { GetUsers } from './'
 
 export default function ViewHackathon() {
-    let { hackID } = useParams();
-    console.log("Obtained ID: " + hackID + " from URL");
+  let { hackID } = useParams();
+  console.log("Obtained ID: " + hackID + " from URL");
 
-    return (
+  return (
     <div className="App">
       <header className="App-header">
         Hackathon ID: {hackID}
-        <GetUsers />
+        <GetUsers hackID={hackID} />
       </header>
     </div>
-    );
+  );
 }
