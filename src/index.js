a89117478e25c44a9520eabcf19af225cc3638ae
Udit Desai <udit.desai3@gmail.com>
2020-05-16 15:04:03 -0400

    Create frontend for registration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import "./fonts/Inter-Bold.ttf";
+import "./fonts/Inter-SemiBold.ttf";
+import "./fonts/Inter-Medium.ttf";
+import "./fonts/Inter-Regular.ttf";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
